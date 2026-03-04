ae7f5d0341d879e2ed1a344b1f618625c4e31b63
khaitbek <khaitbek2005@gmail.com>
2026-03-04 19:50:48 +0500

    add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,40 +6,46 @@ import sanity from "@sanity/astro";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig, envField } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
 
-	integrations: [
-		react(),
-		playformCompress(),
-		sanity({
-			projectId: "vwbu57gz",
-			dataset: "production",
-			useCdn: false,
-			studioBasePath: "/admin",
-		}),
+  integrations: [
+      react(),
+      playformCompress(),
+      sanity({
+          projectId: "vwbu57gz",
+          dataset: "production",
+          useCdn: false,
+          studioBasePath: "/admin",
+      }),
 	],
-	env: {
-		schema: {
-			APP_URL: envField.string({
-				access: "public",
-				context: "client",
-			}),
-			POSTHOG_PROJECT_ID: envField.string({
-				access: "public",
-				context: "client",
-			}),
-			NODE_ENV: envField.string({
-				access: "public",
-				context: "client",
-			}),
-		},
+
+  env: {
+      schema: {
+          APP_URL: envField.string({
+              access: "public",
+              context: "client",
+          }),
+          POSTHOG_PROJECT_ID: envField.string({
+              access: "public",
+              context: "client",
+          }),
+          NODE_ENV: envField.string({
+              access: "public",
+              context: "client",
+          }),
+      },
 	},
-	i18n: {
-		locales: ["en", "uz", "ru"],
-		defaultLocale: "en",
+
+  i18n: {
+      locales: ["en", "uz", "ru"],
+      defaultLocale: "en",
 	},
+
+  adapter: vercel(),
 });
